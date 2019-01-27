e20aa7e2f07a9a28f95d78e7e5c68799882ed6c7
joceycesup <thibault.baron56@gmail.com>
2019-01-27 12:55:38 +0100

    Chara prefab + spawner

---
+++ Assets/Script/HumanSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanSpawner : MonoBehaviour {
+
+	public GameObject prefab;
+	public Transform spawnTransform;
+
+	public float countdown = 0.0f;
+
+	public Rigidbody door1;
+	public Rigidbody door2;
+
+	public Vector3 spawnForce;
+
+	void Start () {
+		StartCoroutine (StartCountdown());
+	}
+	
+	private IEnumerator StartCountdown () {
+		yield return new WaitForSeconds (countdown);
+		Spawn ();
+	}
+
+	protected virtual void Spawn() {
+		if (door1)
+			door1.isKinematic = false;
+		if (door2)
+			door2.isKinematic = false;
+		Rigidbody character = (Instantiate (prefab, spawnTransform.position, spawnTransform.rotation, null)).GetComponentInChildren<Rigidbody>();
+		if (character)
+			character.AddForce (transform.rotation * spawnForce, ForceMode.Impulse);
+	}
+}
